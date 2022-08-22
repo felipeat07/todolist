b3e2d78dfa9a6ce42eed56255099f211b8be3169
Felipe Tavares <felipeat07@gmail.com>
2022-08-22 23:03:12 +0100

    create a status bar and styling input

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,34 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Image, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import { styles } from './styles';
 
 export function Home() {
     return (
         <View style={styles.container}>
-            
+
+            <View style={styles.form}>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Adicione uma nova tarefa"
+                    placeholderTextColor="#6B6B6B"
+                />
+
+                <TouchableOpacity 
+                    style={styles.button}
+                >
+                    <Text style={styles.buttonText}>
+                        <Image 
+                            source={require('../../../assets/plus.png')} 
+                        />
+                    </Text>
+                </TouchableOpacity>
+            </View>
+
+            <View style={styles.status}>
+                <Text style={styles.createStatus}>Criadas</Text>
+                <Text style={styles.finishStatus}>Concluídas</Text>
+            </View>
+
         </View>
     )
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,10 +1,52 @@
 import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
-    container: {
+      container: {
         flex: 1,
         backgroundColor: '#1A1A1A',
-        alignItems: 'center',
-        justifyContent: 'center',
-      }
+      },
+      form: {
+        flexDirection: "row",
+        width: 327,
+        alignSelf: 'center',
+        marginTop: -28,
+      },
+      input: {
+        flex: 1,
+        height: 56,
+        backgroundColor: '#262626',
+        borderRadius: 5,
+        color: '#fff',
+        padding: 16,
+        fontSize: 16,
+        marginRight: 10
+      },
+      buttonText: {
+        color: '#fff',
+        fontSize: 24
+      },
+      button: {
+        width: 56,
+        height: 56,
+        borderRadius: 5,
+        backgroundColor: '#1E6F9F',
+        alignItems: "center",
+        justifyContent: "center"
+      },
+      status: {
+        justifyContent: "space-between",
+        flexDirection: "row",
+        padding: 20,
+        marginTop: 15,
+     },
+     createStatus: {
+      color: '#4EA8DE',
+      fontWeight: "bold",
+      fontSize: 14
+     },
+     finishStatus: {
+      color: '#8284FA',
+      fontWeight: "bold",
+      fontSize: 14
+     }
 })
